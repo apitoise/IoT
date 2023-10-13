71334d469c2579170e35d58b9f9d389299492702
Arthur Pitoiset <arthurpitoiset@MacBook-Air-de-Arthur.local>
2023-10-13 08:57:31 +0200

    debug

--- p1/provision_server.sh
+++ p1/provision_server.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "provision_serv.sh: Provisioning Server..."
-sudo do-release-upgrade
-sudo apt update
-sudo apt install -y systemd
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644" sh -s - --flannel-backend none     --token 4242
+sudo yum install -y net-tools
+systemctl disable firewalld --now
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644" sh -s - --flannel-backend none --token 4242
--- p1/provision_worker.sh
+++ p1/provision_worker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "provision_worker.sh: Provisioning Worker..."
-sudo do-release-upgrade
-sudo apt update
-sudo apt install -y systemd
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://apitoiseS:6443 --toke    n 4242" sh -s -
+sudo yum install -y net-tools
+systemctl disable firewalld --now
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 4242" sh -s -
