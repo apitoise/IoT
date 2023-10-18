39dc672c08434f9c2b2ad69e24089a79835b702c
Arthur Pitoiset <arthurpitoiset@macbook-air-de-arthur.home>
2023-10-18 12:59:06 +0200

    wip

--- p1/provision_server.sh
+++ p1/provision_server.sh
@@ -3,4 +3,7 @@
 echo "provision_serv.sh: Provisioning Server..."
 sudo yum install -y net-tools
 systemctl disable firewalld --now
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644" sh -s - --flannel-backend none --token 4242
+mkdir -p /etc/rancher/k3s/
+openssl genrsa -out /etc/rancher/k3s/k3s-ca-key.pem 2048
+openssl req -x509 -new -nodes -key /etc/rancher/k3s/k3s-ca-key.pem -days 365 -out /etc/rancher/k3s/k3s-ca-cert.pem -subj "/CN=apitoiseS"
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --flannel-backend none --node-ip=\"192.168.56.110\"" sh -s -
