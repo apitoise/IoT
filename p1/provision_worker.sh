720e3b0c79f426539fad447b881d362167250600
apitoise <arthur.pitoiset@outlook.fr>
2023-11-07 12:25:20 +0100

    p2 wip

--- p1/provision_worker.sh
+++ p1/provision_worker.sh
@@ -3,4 +3,4 @@
 echo "provision_worker.sh: Provisioning Worker..."
 sudo apt update
 sudo apt install curl -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server=https://192.168.56.110:6443 --token=$(cat /vagrant/token)" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server=https://192.168.56.110:6443 --token=$(cat /vagrant/token) --node-ip=192.168.56.111" sh -s -
--- p2/provision.sh
+++ p2/provision.sh
@@ -2,4 +2,6 @@
 
 sudo apt update
 sudo apt install curl -y
-sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644" sh -s -
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip=192.168.56.110" sh -s -
+kubectl apply -f /vagrant/deployment.yaml
+kubectl apply -f /vagrant/ingress.yaml
