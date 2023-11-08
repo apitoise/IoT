4ba96fdc8b669431057dec4b6914337d51e65e52
apitoise <arthur.pitoiset@outlook.fr>
2023-11-08 20:09:09 +0100

    wip

--- p2/provision.sh
+++ p2/provision.sh
@@ -3,6 +3,4 @@
 sudo apt update
 sudo apt install curl -y
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip=192.168.56.110" sh -s -
-kubectl apply -f /vagrant/deploy/app1.yaml
-kubectl apply -f /vagrant/deploy/app2.yaml
-kubectl apply -f /vagrant/deploy/app3.yaml
+kubectl apply -f /vagrant/deploy/
